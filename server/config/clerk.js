8ebef50c05a690ccf977000577d7c480afd9ecd8
Wealthometer <wealthhajoh87@gmail.com>
2026-03-28 14:00:01 +0100

    Update clerk.js

--- server/config/clerk.js
+++ server/config/clerk.js
@@ -4,7 +4,6 @@ const { clerkClient } = require("@clerk/express");
 async function verifyClerkSessionToken(token) {
   return verifyToken(token, {
     secretKey: process.env.CLERK_SECRET_KEY,
-    // Limit to known frontends
     authorizedParties: [
       process.env.CLIENT_URL || "http://localhost:5173"
     ],
@@ -15,3 +14,4 @@ module.exports = {
   verifyClerkSessionToken,
   clerkClient,
 };
+
