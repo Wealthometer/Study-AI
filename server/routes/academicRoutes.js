64bb39c60f56fcec90d65fe895b0b3012e415419
Wealthometer <wealthhajoh87@gmail.com>
2026-03-28 14:00:26 +0100

    Update academicRoutes.js

--- server/routes/academicRoutes.js
+++ server/routes/academicRoutes.js
@@ -7,17 +7,15 @@ const {
 } = require("../controllers/academicController");
 const router = express.Router();
 router.use(protect);
-// Tasks
 router.get("/tasks", getTasks);
 router.post("/tasks", createTask);
 router.put("/tasks/:id", updateTask);
 router.delete("/tasks/:id", deleteTask);
-// Subjects
 router.get("/subjects", getSubjects);
 router.post("/subjects", createSubject);
 router.put("/subjects/:id", updateSubject);
 router.delete("/subjects/:id", deleteSubject);
-// Calendar
 router.get("/calendar", getCalendarEvents);
 router.post("/calendar", createCalendarEvent);
 module.exports = router;
+
