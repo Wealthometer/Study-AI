13fdc1274b5a56a14963561651be7eae91ce43be
Wealthometer <wealthhajoh87@gmail.com>
2026-04-09 13:38:19 +0100

    Update Tutor.jsx

--- client/src/pages/Tutor.jsx
+++ client/src/pages/Tutor.jsx
@@ -307,7 +307,7 @@ export default function Tutor() {
         copy[copy.length - 1] = {
           role: "assistant",
           content:
-            "Something went wrong. Please check that your `OPENROUTER_API_KEY` is set in `.env` and try again.",
+            "Something went wrong. Please check that your `GEMINI_API_KEY` is set in `.env` and try again.",
           time: new Date().toLocaleTimeString([], {
             hour: "2-digit",
             minute: "2-digit",
